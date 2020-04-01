a2897acb396a6f9a9fde969a30fea16b2bbdf157
Marek Lewandowski <m.lewandowski@cksource.com>
2020-04-01 10:05:14 +0200

    Script adjusted.

--- scripts/codecov-run-tests.sh
+++ scripts/codecov-run-tests.sh
@@ -3,7 +3,7 @@
 # @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
 # For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
 
-changedPackages=$(git diff master --stat | head -n-1  | awk '{$1=$1};1' | sed -e '/^packages\//!s/.*/ckeditor5/' -e 's#^\s*packages\/ckeditor5\?-\([^\/]\+\).\+#\1#' | sort -u)
+changedPackages=$(git diff origin/master --stat | head -n-1  | awk '{$1=$1};1' | sed -e '/^packages\//!s/.*/ckeditor5/' -e 's#^\s*packages\/ckeditor5\?-\([^\/]\+\).\+#\1#' | sort -u)
 csvChangedPackages=$(echo $changedPackages | sed -e 's/ /,/g')
 
 echo $changedPackages
