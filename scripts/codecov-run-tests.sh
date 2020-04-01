dd0ca3648f0631d297c00c8079cd9b135ed6a4a7
Marek Lewandowski <m.lewandowski@cksource.com>
2020-04-01 10:27:30 +0200

    Script adjusted.

--- scripts/codecov-run-tests.sh
+++ scripts/codecov-run-tests.sh
@@ -7,7 +7,7 @@
 # in case travis is running the script on non-master branch
 git fetch --depth=50 origin refs/heads/master:refs/heads/master
 
-changedPackages=$(git diff origin/master --stat | head -n-1  | awk '{$1=$1};1' | sed -e '/^packages\//!s/.*/ckeditor5/' -e 's#^\s*packages\/ckeditor5\?-\([^\/]\+\).\+#\1#' | sort -u)
+changedPackages=$(git diff master --stat | head -n-1  | awk '{$1=$1};1' | sed -e '/^packages\//!s/.*/ckeditor5/' -e 's#^\s*packages\/ckeditor5\?-\([^\/]\+\).\+#\1#' | sort -u)
 csvChangedPackages=$(echo $changedPackages | sed -e 's/ /,/g')
 
 echo $changedPackages
