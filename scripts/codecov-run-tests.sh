f7258f2b65abd4665fbab33ba573dc41c02a3fe7
Marek Lewandowski <m.lewandowski@cksource.com>
2020-04-17 08:12:43 +0200

    Internal: Since per-package information is folded by default, we can be more verbose and print test run information for both success and failure.

--- scripts/codecov-run-tests.sh
+++ scripts/codecov-run-tests.sh
@@ -19,14 +19,44 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
+# Travis functions inspired by https://github.com/travis-ci/travis-rubies/blob/a10ba31e3f508650204017332a608ef9bce2c733/build.sh.
+function travis_nanoseconds() {
+  local cmd="date"
+  local format="+%s%N"
+  local os=$(uname)
+
+  if hash gdate > /dev/null 2>&1; then
+    cmd="gdate" # use gdate if available
+  elif [[ "$os" = Darwin ]]; then
+    format="+%s000000000" # fallback to second precision on darwin (does not support %N)
+  fi
+
+  $cmd -u $format
+}
+
+travis_time_start() {
+  travis_timer_id=$(printf %08x $(( RANDOM * RANDOM )))
+  travis_start_time=$(travis_nanoseconds)
+  echo -en "travis_time:start:$travis_timer_id\r${ANSI_CLEAR}"
+}
+
+travis_time_finish() {
+  local result=$?
+  travis_end_time=$(travis_nanoseconds)
+  local duration=$(($travis_end_time-$travis_start_time))
+  echo -en "\ntravis_time:end:$travis_timer_id:start=$travis_start_time,finish=$travis_end_time,duration=$duration\r${ANSI_CLEAR}"
+  return $result
+}
+
+
 fold_start() {
-  echo -e "travis_fold:start:$1$2"
+  echo -e "travis_fold:start:$1 $2"
   travis_time_start
 }
 
 fold_end() {
   travis_time_finish
-  echo -e "\ntravis_fold:end:$1\r"
+  echo -e "\ntravis_fold:end:$1\n"
 
 }
 
